1eca79f9268221fe986b519d64591c68bd8047ac
FerroO2000 <immersivetech.mod@gmail.com>
2023-06-29 13:12:26 +0200

    json unmarshall errors with line #5

--- pkg/json.go
+++ pkg/json.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -28,6 +29,26 @@ func NewJsonReader() *JsonReader {
 	return &JsonReader{}
 }
 
+func (r *JsonReader) getLineErr(input []byte, offset int, jsonErr error) error {
+	lf := rune(0x0A)
+
+	if offset > len(input) || offset < 0 {
+		return fmt.Errorf("couldn't find offset %d within the input", offset)
+	}
+
+	line := 1
+	for i, b := range input {
+		if b == byte(lf) {
+			line++
+		}
+		if i == offset {
+			break
+		}
+	}
+
+	return fmt.Errorf("line %d: %v", line, jsonErr)
+}
+
 func (r *JsonReader) Read(file *os.File) (*CanModel, error) {
 	jsonFile, err := ioutil.ReadAll(file)
 	if err != nil {
@@ -35,7 +56,15 @@ func (r *JsonReader) Read(file *os.File) (*CanModel, error) {
 	}
 
 	canModel := &CanModel{}
-	if err := json.Unmarshal(jsonFile, canModel); err != nil {
+	err = json.Unmarshal(jsonFile, canModel)
+	if err != nil {
+		switch jsonErr := err.(type) {
+		case *json.UnmarshalTypeError:
+			return nil, r.getLineErr(jsonFile, int(jsonErr.Offset), jsonErr)
+		case *json.SyntaxError:
+			return nil, r.getLineErr(jsonFile, int(jsonErr.Offset), jsonErr)
+		}
+
 		return nil, err
 	}
 
